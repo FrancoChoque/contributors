d079912ba878ea3c7a7591e63065703f76f7ac80
Franco Choque <franco.choquescipione@gmail.com>
2021-09-01 10:14:03 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+
+const StyledContainer = styled.ul`
+  margin: 10px;
+`
+
+const StyledItem = styled.li`
+  margin: 5px 0;
+`
+
+
 function App() {
+const [contributors, setContributors] = useState([]);
+const [error,setError] = useState('');
+useEffect(() => {
+  setError('');
+  axios.get('https://api.github.com/repos/facebook/react/contributors')
+    .then((res) => setContributors(res.data))
+    .catch((err) => {
+      console.log(err)
+      setError('An error happened');
+    });  
+}, []);
+
+const sortedContributors = [...contributors].sort((a,b) => a.login.toLowerCase() > b.login.toLowerCase());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {error ? error : <StyledContainer>{sortedContributors.map(each => <StyledItem key={each.login}>{each.login}</StyledItem>)}</StyledContainer>}    
     </div>
   );
 }
